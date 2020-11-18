e790ee9f25986c9858c53e4e5e4a92db790ab124
A.B <52319983+arjenab@users.noreply.github.com>
2020-11-18 17:19:20 +0100

    sms now waiting on module

--- smsTest/smsTest/smsTest.ino
+++ smsTest/smsTest/smsTest.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 2); //A6 Tx & Rx is connected to Arduino #3 & #2
 int trigPin = 11;
 int echoPin = 12;
 int mailFlap=20;
@@ -5,9 +8,18 @@ long duration, cm;
 
 void setup() {
     Serial.begin(9600);
+    mySerial.begin(9600);
     pinMode(trigPin,OUTPUT);
     pinMode(echoPin, INPUT);  
-
+    Serial.println("Initializing..."); 
+    delay(1000);
+  
+    mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+    updateSerial();
+    mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+    updateSerial();
+    mySerial.println("AT+CMGS=\"+31629093188\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+    updateSerial();
 }
 
 void loop() {
@@ -34,5 +46,21 @@ void checkMail() {
 }
 
 void sendMessage() {
-  Serial.println("send a message");
+    mySerial.print("You've got mail"); //text content
+    // mySerial.print("Last Minute Engineers | lastminuteengineers.com"); //text content
+    updateSerial();
+    mySerial.write(26);
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
 }
