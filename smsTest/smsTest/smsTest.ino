ba6e531622d63a18b9dcff65392255bb17cd3353
A.B <52319983+arjenab@users.noreply.github.com>
2020-11-24 14:54:36 +0100

    working on reciever

--- smsTest/smsTest/smsTest.ino
+++ smsTest/smsTest/smsTest.ino
@@ -1,49 +1,32 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(3, 2); //A6 Tx & Rx is connected to Arduino #3 & #2
-int trigPin = 11;
-int echoPin = 12;
-int mailFlap=20;
-long duration, cm;
-
+int rate = 9600;
 void setup() {
-    Serial.begin(9600);
-    mySerial.begin(9600);
-    pinMode(trigPin,OUTPUT);
-    pinMode(echoPin, INPUT);  
+    Serial.begin(rate);
+    mySerial.begin(rate);
+
     Serial.println("Initializing..."); 
     delay(1000);
-  
+
     mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
     updateSerial();
+        mySerial.println("AT+CPIN=0000");
+    delay(1000);
+//    mySerial.write(26);
     mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
     updateSerial();
-    mySerial.println("AT+CMGS=\"+31629093188\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+    mySerial.println("AT+CTZR?");
+    updateSerial();
+    mySerial.println("AT+CNMI=1,2,0,0,0"); // Decides how newly arrived SMS messages should be handled
     updateSerial();
 }
 
 void loop() {
-  setEcho();
+   updateSerial();
+
 }
 
-void setEcho(){
- digitalWrite(trigPin,LOW);
- delayMicroseconds(5);
- digitalWrite(trigPin,HIGH);
- delayMicroseconds(10);
- digitalWrite(trigPin,LOW);
- pinMode(echoPin, INPUT);
- duration=pulseIn(echoPin,HIGH);
- cm=(duration/2)/29.1;
-}
-
-void checkMail() {
-   if(cm<=mailFlap){
-      sendMessage();
-   } else{ 
-    
-  }
-}
 
 void sendMessage() {
     mySerial.print("You've got mail"); //text content
